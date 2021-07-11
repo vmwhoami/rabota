d0b40530eccdce4e7f340bc626e5de0797f44b61
Vitalie Melnic <vmwhoami@gmail.com>
2021-07-11 20:39:14 +0300

    Fix linting errors

--- dist/sendCV.js
+++ dist/sendCV.js
@@ -11,12 +11,12 @@ const uploadCV = async (fileExists, elementHandle) => {
 };
 const SendCV = async (page) => {
     await page.waitForTimeout(500);
-    let spans = await page.$$('form .tabs span');
+    const spans = await page.$$('form .tabs span');
     await spans[0].click();
     await page.waitForTimeout(500);
     await page.waitForSelector('#full_name');
-    await page.type("#full_name", "Vitalie Melnic");
-    const elementHandle = await page.$("input[type=file]");
+    await page.type('#full_name', 'Vitalie Melnic');
+    const elementHandle = await page.$('input[type=file]');
     await uploadCV(fileExists, elementHandle);
     await page.click('#agreement-id-styler');
     await page.waitForSelector('.set_cookie_agreement');
--- src/sendCV.ts
+++ src/sendCV.ts
@@ -5,25 +5,25 @@ const fileExists = fs.existsSync(`${pathFile}/VITALIEMELNIC.pdf`);
 
 const uploadCV = async (fileExists: any, elementHandle: any) => {
   if (fileExists) {
-    await elementHandle.uploadFile(`${pathFile}/VITALIEMELNIC.docx`)
+    await elementHandle.uploadFile(`${pathFile}/VITALIEMELNIC.docx`);
   }
-}
+};
 
 const SendCV = async (page: any) => {
   await page.waitForTimeout(500);
-  let spans: any = await page.$$('form .tabs span');
+  const spans: any = await page.$$('form .tabs span');
   await spans[0].click();
   await page.waitForTimeout(500);
   await page.waitForSelector('#full_name');
-  await page.type("#full_name", "Vitalie Melnic");
-  const elementHandle = await page.$("input[type=file]");
-  await uploadCV(fileExists, elementHandle)
+  await page.type('#full_name', 'Vitalie Melnic');
+  const elementHandle = await page.$('input[type=file]');
+  await uploadCV(fileExists, elementHandle);
   await page.click('#agreement-id-styler');
   await page.waitForSelector('.set_cookie_agreement');
   await page.waitForTimeout(500);
   await page.click('.set_cookie_agreement');
   await page.waitForTimeout(1500);
   await page.click('#cv_sent .m_window .mw_close');
-}
+};
 
 export default SendCV;
