dcc256794d831778e1f89acf6df54730dfa05547
vmwhoami <vmwhoami@gmail.com>
2025-01-27 14:07:04 +0200

    update old dependancies to a slightly newer one

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,30 @@
 /* eslint @typescript-eslint/no-var-requires: "off" */
-require('dotenv').config({path: '.env'})
+import 'dotenv/config'; // Changed from require to import
 import { startBrowser } from './startCloseBrowser';
 import login from './login';
 import applyToJobs from "./applyToJobs";
 import risetoTop from './rizeTop';
 
-const gotToAndDo = async (url: string,search:string,rizeTop:boolean) => {
+const gotToAndDo = async (url: string, search: string, rizeTop: boolean) => {
   const email = process.env.EMAIL;
   const password = process.env.PASSWORD;
   const { page } = await startBrowser();
   page.setViewport({ width: 1000, height: 800 });
   await page.goto(url);
   await login(page, email, password);
-  if(rizeTop){ 
-  await risetoTop(page);
+  if(rizeTop) { 
+    await risetoTop(page);
   }
   await page.goto(url + search);
   await page.setViewport({ width: 500, height: 1000 });
   await page.addStyleTag({ content: "* {scroll-behavior: auto !important;}" });
-  await applyToJobs(page)
-
+  await applyToJobs(page);
 }
 
 const url = "https://www.rabota.md/ro/";
 const searchCat = 'jobs-moldova-developer';
-const rizeTop =true;
+const rizeTop = true;
 
 (async () => {
-  await gotToAndDo(url,searchCat,rizeTop);
+  await gotToAndDo(url, searchCat, rizeTop);
 })();
